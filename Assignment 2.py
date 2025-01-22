fb47c03beddb6780b069aafdabd449eaf9dea10a
kirkbeddoe <kirkbeddoe@gmail.com>
2025-01-22 17:47:11 -0500

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,13 @@
+# prompt the user to enter the length and width of a rectangle.
+
+length = float(input("enter the length: "))
+width = float(input("enter the width: "))
+
+# calculate the area of a rectangle using the formula: area = length * width
+
+area = length * width
+
+# output
+
+print("Your area is: ", area)
+
