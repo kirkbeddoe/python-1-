77eefc89c64122c1a7aa28a016a3c652c6c7af52
kirkbeddoe <kirkbeddoe@gmail.com>
2025-01-23 10:17:06 -0500

    Add files via upload

---
+++ assignment 4.py
@@ -0,0 +1,23 @@
+# Input: Prompt the user to enter the coordinates of two points in a 2D plane (x1, y1) and (x2, y2).
+# Processing: Calculate the distance between the two points using the distance formula: Distance = sqrt((x2 - x1)^2 + (y2 - y1)^2).
+# Output: Display the calculated distance between the two points.
+
+#input
+x1 = float (input("Enter x1 coordinate: "))
+y1 = float (input("Enter y1 coordinate: "))
+x2 = float (input("Enter x2 coordinate: "))
+y2 = float (input("Enter y2 coordinate: "))
+
+#processing
+dx_squared = (x2 - x1) ** 2
+dy_squared = (y2 - y1) ** 2
+
+#adding the distance squared
+distance_squared = dx_squared + dy_squared
+
+#calculate the distance
+distance = distance_squared ** 0.5
+
+#output
+print("The distance between the points is:", distance)
+
